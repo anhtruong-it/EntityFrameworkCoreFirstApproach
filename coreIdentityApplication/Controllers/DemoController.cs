bffa75dbf1dc9cfe5bef1a3b2c61b40b883884dc
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-07-18 01:09:31 +1000

    add authentication

---
+++ coreIdentityApplication/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreIdentityApplication.Controllers
+{
+    public class DemoController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Details()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Policy()
+        {
+            return View();
+        }
+    }
+}
